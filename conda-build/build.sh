93a073b64b86719d5b75f18470eb132cbcd6901a
Jian Wang <jw3132@columbia.edu>
2019-05-08 11:39:33 -0400

    build work

--- conda-build/build.sh
+++ conda-build/build.sh
@@ -1,4 +1,4 @@
 
 
-conda install pytorch torchvision cudatoolkit=10.0 -c pytorch
+#conda install pytorch torchvision cudatoolkit=10.0 -c pytorch
 $PYTHON setup.py install     # Python command to install the script.
--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 import os
 import sys
-import setuptools
-# from setuptools import setup
+from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
 ENV = os.environ["CONDA_PREFIX"]  # Absolute path of active conda env root
 LIBRARY_DIRS = [os.path.join(ENV, "lib")]  # .[so,dylib]
@@ -15,35 +11,13 @@ ext_modules = []
 if '--with-extension' in sys.argv:
     ext_modules.append(
         CUDAExtension('cuhals',
-                      ['./locanmf/cuhals.cpp', './locanmf/cuhals_kernels.cu'],
+                      ['cuhals.cpp', 'cuhals_kernels.cu'],
                       extra_compile_args={'cxx': ["-fopenmp", "-I"+INCLUDE_DIRS[0]],
                                           'nvcc': []}))
     sys.argv.remove('--with-extension')
 
-# setup(name='locaNMF',
-#       ext_modules=ext_modules,
-#       cmdclass={
-#           'build_ext': BuildExtension
-#       })
-
-
-setuptools.setup(
-    name="locanmf",
-    version="1.0",
-    author="Ian August Kinsella",
-    author_email="iak2199@columbia.edu",
-    description="Demixer using Localized semi-Nonnegative Matrix Factorization method",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ikinsella/locaNMF",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    ext_modules=ext_modules,
-    cmdclass={
-        'build_ext': BuildExtension
-    },
-)
+setup(name='locaNMF',
+      ext_modules=ext_modules,
+      cmdclass={
+          'build_ext': BuildExtension
+      })
