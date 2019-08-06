090b8fb68d249edce539c5d93cd23ee9359d6eb6
Jian Wang <jw3132@columbia.edu>
2019-08-06 10:22:08 -0400

    p-w: init_w function uses too much memory

--- locanmf/cnmf_e.py
+++ locanmf/cnmf_e.py
@@ -49,8 +49,9 @@ def spatial_filtering(spatial_u, settings):
     return spatial_u
 
 
-def pnr(Y, setting):
-    """Calculate PNR (Peak Noise Ratio) of image data Y
+def pnr(spatial_u, setting):
+    """Calculate PNR (Peak Noise Ratio) of denoised image data spatial_u
+
 
     :return: PNR of image data Y
     """
@@ -68,4 +69,122 @@ def correlation(Y, setting):
     "function Cn = correlation_image(Y, sz, d1, d2, flag_norm, K)"
 
     pass
+
+
+def update_ring_model_w(U, V, A, X, W, d1, d2, T, r):
+    """Update W matrix using ring model
+
+    :param U: spatial component matrix from denoiser, R(d=d1xd2, N)
+    :param V: temporal component matrix from denoiser, R(N, T)
+    :param A: spatial component matrix, R(d, K)
+    :param X: temporal component matrix (the actual temporal matrix C = XV),  R(K, N)
+    :param W: weighting matrix, R(d, d)
+    :param d1: x axis frame size
+    :param d2: y axis frame size
+    :param T: number to time steps along time axis
+    :param r: ring radius of ring model
+    :return:
+        W: updated weighting matrix
+        b0: constant baseline of background image
+    """
+
+    if not W:
+        W = init_w(d1, d2, r)
+
+    dU = U - np.matmul(A, X)
+    b0 = np.matmul(dU, np.mean(V, axis=1)/T)
+    W = update_w_1p(dU, W)
+    return b0, W
+
+
+def init_w(d1, d2, r):
+    """Compute the initial W weighting matrix
+
+    :param d1: x axis frame size
+    :param d2: y axis frame size
+    :param r: ring radius of ring model
+    :return:
+    """
+
+    # Compute XY distance tile
+    x_tile = np.tile(range(-(r + 1), (r + 2)), [(2 * r + 3), 1])
+    y_tile = np.transpose(np.tile(range(-(r + 1), (r + 2)), [(2 * r + 3), 1]))
+    xy_tile = np.sqrt(np.multiply(x_tile, x_tile) + np.multiply(y_tile, y_tile))
+
+    # Find ring index
+    r_tile = np.ones((2 * r + 3, 2 * r + 3)) * r
+    r1_tile = r_tile + 1
+    ring = np.logical_and(np.greater_equal(xy_tile, r_tile),
+                          np.less(xy_tile, r1_tile))
+    ring_idx = np.argwhere(ring)
+    ring_idx_T = np.transpose(ring_idx)
+    ring_idx_T = ring_idx_T - (r + 1) # shift index so that center has zero index
+
+    # Create a weighting matrix to store initial value, the matrix size is padded
+    # r cells along 2nd and 3rd dimensions to avoid out of index
+    d_d1_d2_2r = np.zeros((d1 * d2, d1 + 2 * r + 3, d2 + 2 * r + 3))
+    for i in range(d1):
+        for j in range(d2):
+            ij = i * d2 + j
+            x_base = i + r
+            y_base = j + r
+            d_d1_d2_2r[ij, ring_idx_T[0, :] + x_base, ring_idx_T[1, :] + y_base] = 1
+
+    # Grep the central part and reshape
+    d3_sub = d_d1_d2_2r[:, r:(r + d1), r:(r + d2)]
+    W = np.reshape(d3_sub, (d1*d2, -1))
+    return W
+
+
+def update_w_1p(dU, W, d1, d2):
+    """Update weighting matrix W in place
+
+    :param dU: 2D matrix that is equal to U - AX
+    :param W: weighting matrix
+    :param d1: x axis frame size
+    :param d2: y axis frame size
+    :return: None,
+    """
+
+    d = d1 * d2
+    for i in range(d):
+        dU = dU[i, :]
+        omega = W[i, :] > 0  #use greater than 0, instead of not-equal to 0
+        X = dU[omega, :] # omega is row index vector, check whether this OK
+        A = np.matmul(X, np.transpose(X))
+        b = np.matmul(X, np.transpose(dU))
+        W[i, omega] = np.linalg.solve(A, b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- locanmf/test_cnmf_e.py
+++ locanmf/test_cnmf_e.py
@@ -1,12 +1,10 @@
 
-
-
 import numpy as np
 from scipy import signal
 from scipy import ndimage
 import scipy.io as sio
 
-from .cnmf_e import spatial_filtering
+from .cnmf_e import spatial_filtering, init_w, update_ring_model_w
 
 
 def test_spatial_filtering():
@@ -19,3 +17,34 @@ def test_spatial_filtering():
     spatial_u_filtered = spatial_filtering(spatial_u, settings)
     sio.savemat('/mnt/data/test_CNMFE/from_python.mat', {'spatial_u_filtered': spatial_u_filtered})
     # pass
+
+
+def test_init_w():
+    d1, d2, r = 41, 42, 13
+    w = init_w(d1, d2, r)
+    print(w.shape)
+
+
+def test_update_ring_model_w():
+    data = np.load("init_W.npz")
+    print(data.files)
+    d1, d2, T, r = data['arr_4']
+    # print(d1, d2, r, T)
+    U = data['arr_0']
+    V = data['arr_1']
+    A = data['arr_2']
+    X = data['arr_3']
+    print(U.shape, V.shape, A.shape, X.shape)
+
+
+    b0, W = update_ring_model_w(U, V, A, X, [], d1, d2, T, r)
+    print(b0.shape, W.shape)
+
+
+    pass
+
+
+
+
+if __name__ == "__main__":
+    test_update_ring_model_w()
